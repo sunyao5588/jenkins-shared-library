f6164b35fd8c48bae39b04f84abe0fa2599dd8fd
sunyao <yao.yc.sun@cn.pwc.com>
2019-11-25 14:58:45 +0800

    Create cancelPreviousBuilds.groovy

---
+++ vars/cancelPreviousBuilds.groovy
@@ -0,0 +1,15 @@
+def cancelPreviousBuilds() {
+    def jobName = env.JOB_NAME
+    def buildNumber = env.BUILD_NUMBER.toInteger()
+    /* Get job name */
+    def currentJob = Jenkins.instance.getItemByFullName(jobName)
+
+    /* Iterating over the builds for specific job */
+    for ( build in currentJob.builds) {
+        /* If there is a build that is currently running and it's not current build */
+        if (build.isBuilding() && build.getNumber().toInteger() != buildNumber) {
+            /* Than stopping it */
+            build.doStop()
+        }
+    }
+}
