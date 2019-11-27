a9bfb959b6addddc079ca0ad23189d889063fc0f
sunyao5588 <yao.yc.sun@cn.pwc.com>
2019-11-27 10:47:37 +0800

    add commitSha

--- vars/setBuildStatus.groovy
+++ vars/setBuildStatus.groovy
@@ -1,7 +1,8 @@
-void call(String giturl, String gitcontext, String message, String state) {
+void call(String commitSha, String giturl, String gitcontext, String message, String state) {
   step([
       $class: "GitHubCommitStatusSetter",
       reposSource: [$class: "ManuallyEnteredRepositorySource", url: giturl],
+      commitShaSource: [$class: "ManuallyEnteredShaSource", sha: commitSha],
       contextSource: [$class: "ManuallyEnteredCommitContextSource", context: gitcontext],
       errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
       statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
