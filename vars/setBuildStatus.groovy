29d4693c14b0dbccdf1f44485db7670ed9fd8aa2
sunyao5588 <yao.yc.sun@cn.pwc.com>
2019-11-27 16:23:28 +0800

    add repoUrl var

--- vars/setBuildStatus.groovy
+++ vars/setBuildStatus.groovy
@@ -1,13 +1,19 @@
+def getRepoURL() {
+  sh "git config --get remote.origin.url > .git/remote-url"
+  return readFile(".git/remote-url").trim()
+}
+
 def getCommitSha() {
   sh "git rev-parse HEAD > .git/current-commit"
   return readFile(".git/current-commit").trim()
 }
 
-void call(String giturl, String gitcontext, String message, String state) {
+void call(String gitcontext, String message, String state) {
+  repoUrl = getRepoURL()
   commitSha = getCommitSha()
   step([
       $class: "GitHubCommitStatusSetter",
-      reposSource: [$class: "ManuallyEnteredRepositorySource", url: giturl],
+      reposSource: [$class: "ManuallyEnteredRepositorySource", url: repoUrl],
       commitShaSource: [$class: "ManuallyEnteredShaSource", sha: commitSha],
       contextSource: [$class: "ManuallyEnteredCommitContextSource", context: gitcontext],
       errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
